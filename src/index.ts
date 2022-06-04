77057a671019948a14055400380372bae99c2b0a
akswnd98 <akswnd98@gmail.com>
2022-06-04 23:58:46 +0900

    session 유지 시간 1시간으로

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,10 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false },
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60,
+  },
 }));
 
 app.use('/api', api);
